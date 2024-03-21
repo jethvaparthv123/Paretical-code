9b58675b2d225105271d0a2313c621f15a5fbae7
jethvaparthv123 <161803065+jethvaparthv123@users.noreply.github.com>
2024-03-21 15:13:07 +0530

    Add files via upload

---
+++ paretical 2.1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int num1,num2;
+cout << "enter two integers" << endl;
+cin >> num1>>num2;
+int sum=num1+num2;
+cout<<num1<<"+"<<num2<<"="<<sum<<endl;
+int difference=num1-num2;
+cout<<num1<<"-"<<num2<<"="<<difference<<endl;
+int multi=num1*num2;
+cout<<num1<<"x"<<num2<<"="<<multi<<endl;
+int div=num1/num2;
+cout<<num1<<"÷"<<num2<<"="<<div<<endl;
+
+}
